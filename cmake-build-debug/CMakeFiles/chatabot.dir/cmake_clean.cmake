8b1bb948b8f2d91189f7c05d0bb1c458909c1a43
CristianFaur <56438870+CristianFaur@users.noreply.github.com>
2019-10-17 23:06:12 +0300

    never commit

---
+++ cmake-build-debug/CMakeFiles/chatabot.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chatabot.dir/main.c.obj"
+  "chatabot.exe"
+  "chatabot.exe.manifest"
+  "chatabot.pdb"
+  "libchatabot.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chatabot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
